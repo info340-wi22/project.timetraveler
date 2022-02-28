79b4ec613749a71cf46157bb000ada4343da2b5e
HongjieWang0824 <hongjiew@uw.edu>
2022-02-28 13:15:33 -0800

    fix about page

--- src/components/App.js
+++ src/components/App.js
@@ -17,24 +17,12 @@ function App(props) {
     <div>
       <NavBar />
       <main>
-<<<<<<< HEAD
-
-
         <Routes>
           <Route path="/" element={<HomePage pets={pets}/>} />
           <Route path="countdown" element={<Countdown />} />
           <Route path="about" element={<AboutPage />} />
         </Routes>
 
-=======
-        {/* <Routes>
-          <Route path="home" element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="countdown" element={<CountdownPage/>} />
-        </Routes> */}
-        <Countdown />
-        {/* <HomePage /> */}
->>>>>>> 089c1589253ea2f973b44a382ddc1638640895ed
       </main>
       <footer className="container">
         <small>&copy; INFO340WI22 | Group B1 </small>
--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -18,15 +18,6 @@ export default function Countdowm(props) {
                     />
                 </div>
             </main>
-<<<<<<< HEAD
-=======
-            {/* <footer>
-                <div className="footer">
-                    <small><a href="mailto:yujias6@uw.edu"><span className="material-icons"></span> </a></small>
-                    <small>&copy; TimeTraveler Team 2022</small>
-                </div>
-            </footer> */}
->>>>>>> 089c1589253ea2f973b44a382ddc1638640895ed
         </div>
     );
 }
